50dd489d6952c03f644da9cc7839eda970cfb950
MaliNeca <borcanac.c1@gmail.com>
2020-11-18 18:21:34 +0100

    v1.1

---
+++ Assets/Scripts/LoadParams.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadParams : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Debug.Log(LoadScene.playerID);
+    }
+    
+}
---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    static public int playerID;
+    
+    public void startGame(int pID)
+    {
+        playerID = pID;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        
+    }
+}
